b50f2af7a871be399155c7e9fa8471ef4f0f8dd3
Morten Delenk <mtg.morten@googlemail.com>
2016-07-27 15:09:21 +0200

    oh

--- ctype/_ctype.h
+++ ctype/_ctype.h
@@ -3,8 +3,8 @@
 #define UPPER 4
 #define LOWER 8
 #define NUMBER 16
-#define OPEN 32|SYMBOL
-#define CLOSE 64|SYMBOL
+#define OPEN (32|SYMBOL)
+#define CLOSE (64|SYMBOL)
 #define SPACE 128
-#define HEXNUM 256|UPPER
+#define HEXNUM (256|UPPER)
 extern int ascii_table[256];
