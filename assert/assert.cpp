20e1252f7134c07b6f58aa1d09d13fb5aca201b2
Morten Delenk <mtg.morten@googlemail.com>
2016-07-27 12:14:59 +0200

    file didn't exist

--- assert/assert.cpp
+++ assert/assert.cpp
@@ -1,6 +1,6 @@
 #include <assert.h>
 #include <base.hpp>
-#include <text_DISP.hpp>
+#include <textDISP.hpp>
 void "C" __assert(const char *msg, const char *file, int line, int res) {
     if(!res) {
         MTGosHAL::err << "assert(" << msg << ") failed in " << file << " at line " << line << "!\n";
