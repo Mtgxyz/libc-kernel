1699c8693a323878e67246783748eb77a2d4a4fd
Morten Delenk <mtg.morten@googlemail.com>
2016-07-27 15:10:18 +0200

    wat

--- ctype/isAlpha.cpp
+++ ctype/isAlpha.cpp
@@ -1,7 +1,7 @@
 #include <ctype.h>
 #include "_ctype.h"
 #include <assert.h>
-extern "C" isalpha(int c) {
+extern "C" int isalpha(int c) {
     assert(c<256);
     return ascii_table[c] & (UPPER|LOWER);
 }
